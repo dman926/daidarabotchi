4f4d3e71a30a8ed85f866ecfb1f833e0033dbc66
Dennis James Stelmach <dj@dstelmach.com>
2022-03-27 16:07:44 -0400

    remove boilerplate

--- apps/portfolio/app/routes/index.tsx
+++ apps/portfolio/app/routes/index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction, LoaderFunction } from 'remix';
 import { useLoaderData, json, Link } from 'remix';
+import Header from '~/components/resume/header/header';
 
 type IndexData = {
   resources: Array<{ name: string; url: string }>;
@@ -60,41 +61,7 @@ export default function Index() {
 
   return (
     <div className="remix__page">
-      <main>
-        <h2>Welcome to Remix!</h2>
-        <p>We're stoked that you're here. 🥳</p>
-        <p>
-          Feel free to take a look around the code to see how Remix does things,
-          it might be a bit different than what you’re used to. When you're
-          ready to dive deeper, we've got plenty of resources to get you
-          up-and-running quickly.
-        </p>
-        <p>
-          Check out all the demos in this starter, and then just delete the{' '}
-          <code>app/routes/demos</code> and <code>app/styles/demos</code>{' '}
-          folders when you're ready to turn this into your next project.
-        </p>
-      </main>
-      <aside>
-        <h2>Demos In This App</h2>
-        <ul>
-          {data.demos.map((demo) => (
-            <li key={demo.to} className="remix__page__resource">
-              <Link to={demo.to} prefetch="intent">
-                {demo.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h2>Resources</h2>
-        <ul>
-          {data.resources.map((resource) => (
-            <li key={resource.url} className="remix__page__resource">
-              <a href={resource.url}>{resource.name}</a>
-            </li>
-          ))}
-        </ul>
-      </aside>
+      <Header />
     </div>
   );
 }
