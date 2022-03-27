e81c3d066c8f7b5ee0b0d2765b298f6638ba55e5
Dennis James Stelmach <dj@dstelmach.com>
2022-03-27 13:56:09 -0400

    replace react portfolio with next.js

--- apps/portfolio/src/app/app.tsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import { Layout } from '@daidarabotchi/portfolio-lib';
-
-export function App() {
-  return <Layout />;
-}
-
-export default App;
