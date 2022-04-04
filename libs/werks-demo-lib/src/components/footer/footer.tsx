8b452454cb9422993a2b8826dc773577739f0e59
Dennis James Stelmach <dj@dstelmach.com>
2022-04-04 00:28:47 -0400

    fill out footer

--- libs/werks-demo-lib/src/components/footer/footer.tsx
+++ libs/werks-demo-lib/src/components/footer/footer.tsx
@@ -1,13 +1,28 @@
+import { AppBar, Grid, Toolbar } from '@daidarabotchi/material-ui';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './footer.module.scss';
 
 /* eslint-disable-next-line */
-export interface FooterProps {}
+export interface FooterProps { }
 
 export function Footer(props: FooterProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Footer!</h1>
-    </div>
+    <AppBar position='static'>
+      <Toolbar>
+        <Grid container justifyContent="center" alignItems="center" spacing={3}>
+          <Grid item xs={4}>
+            NAV ITEMS
+          </Grid>
+          <Grid item xs={4}>
+            BLOG/DEAL ITEMS?
+          </Grid>
+          <Grid item xs={4}>
+            ACCOUNT ITEMS
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
 
