3a0da0d26417b1837e417e9f1aafd149ea06b354
Dennis James Stelmach <dj@dstelmach.com>
2022-04-04 00:23:10 -0400

    fill out header

--- apps/werks-demo/pages/_app.tsx
+++ apps/werks-demo/pages/_app.tsx
@@ -1,12 +1,27 @@
+import { Box } from '@daidarabotchi/material-ui';
+import { Footer, Header } from '@daidarabotchi/werks-demo-lib';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../store';
+import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const theme = createTheme({});
+
   return (
-   <Provider store={store}>
-     <Component {...pageProps} />
-   </Provider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Header />
+          <main id="content" style={{ flexGrow: 1 }}>
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </Box>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
--- libs/werks-demo-lib/src/index.ts
+++ libs/werks-demo-lib/src/index.ts
@@ -1,2 +1,5 @@
+export * from './components/header/header';
+export * from './components/footer/footer';
+
 export * from './pages/index/index';
 export * from './pages/checkout/checkout';
