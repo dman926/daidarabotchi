d9648fcabde71df70eb273fb349f4f4ca2c6a36f
Dennis James Stelmach <dj@dstelmach.com>
2022-04-23 21:43:33 -0400

    remove call-to-action scss

--- libs/new-england-keeshonds-lib/src/components/home/call-to-action/call-to-action.tsx
+++ libs/new-england-keeshonds-lib/src/components/home/call-to-action/call-to-action.tsx
@@ -1,11 +1,9 @@
-import styles from './call-to-action.module.scss';
-
 /* eslint-disable-next-line */
 export interface CallToActionProps {}
 
 export function CallToAction(props: CallToActionProps) {
   return (
-    <div className={styles['container']}>
+    <div>
       <h1>Welcome to CallToAction!</h1>
     </div>
   );
