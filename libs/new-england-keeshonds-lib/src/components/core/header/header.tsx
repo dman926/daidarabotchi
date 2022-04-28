5f5c2de287ead4bd46e1f00b8da8e79c269670da
Dennis James Stelmach <dj@dstelmach.com>
2022-04-28 18:57:46 -0400

    better center items

--- libs/new-england-keeshonds-lib/src/components/core/header/header.tsx
+++ libs/new-england-keeshonds-lib/src/components/core/header/header.tsx
@@ -3,11 +3,18 @@ import {
   Button,
   ButtonGroup,
   ButtonProps as MuiButtonProps,
+  Grid,
   Toolbar,
   Typography,
 } from '@daidarabotchi/material-ui';
+import { styled } from '@mui/material/styles';
 import { ReactNode } from 'react';
 import { NavigateOptions } from 'react-router-dom';
+
+const GridCenterContents = styled(Grid)(() => ({
+  display: 'grid',
+  placeItems: 'center',
+}));
 
 export interface HeaderMenuItemProps {
   link: string;
@@ -28,50 +35,65 @@ export function Header(props: HeaderProps) {
   return (
     <AppBar position="static" data-testid="nek-header">
       <Toolbar>
-        <Typography
-          variant="h1"
-          onClick={() => navigate(home)}
-          sx={{
-            fontSize: '1em',
-            cursor: currentRoute === home ? 'default' : 'pointer',
-            display: 'flex',
-            alignItems: 'center',
-            gap: 1
-          }}
-          data-testid="nek-header-title"
-        >
-          {typeof title === 'string' ? (
-            title
-          ) : (
-            <>
-              {title.icon}
-              <span>{title.text}</span>
-            </>
-          )}
-        </Typography>
-        <span style={{ flexGrow: 1 }} />
-        <ButtonGroup
-          sx={{
-            display: 'grid',
-            gridTemplateColumns: `repeat(${menu.length}, 1fr)`,
-          }}
-          data-testid="nek-header-menu"
-        >
-          {menu.map((menuItem) => {
-            const { link, text, ButtonProps = {} } = menuItem;
-            return (
-              <Button
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                {...ButtonProps}
-                onClick={() => navigate(link)}
-                key={text}
-                data-testid="nek-header-nav-button"
-              >
-                <Typography>{text}</Typography>
-              </Button>
-            );
-          })}
-        </ButtonGroup>
+        <Grid container spacing={2}>
+          <GridCenterContents item xs={6}>
+            <Typography
+              variant="h1"
+              onClick={() => navigate(home)}
+              sx={{
+                fontSize: '1em',
+                cursor: currentRoute === home ? 'default' : 'pointer',
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 1,
+              }}
+              data-testid="nek-header-title"
+            >
+              {typeof title === 'string' ? (
+                title
+              ) : (
+                <>
+                  {title.icon}
+                  <span>{title.text}</span>
+                </>
+              )}
+            </Typography>
+          </GridCenterContents>
+          <GridCenterContents
+            item
+            xs={1}
+            sx={{ position: 'absolute' }}
+          >
+            {/* @TODO: PUT AN ANNOUNCEMENT BAR HERE WHEN THERE IS A CURRENT LITTER */}
+          </GridCenterContents>
+          <GridCenterContents item xs={6}>
+            <ButtonGroup
+              sx={{
+                display: 'grid',
+                gridTemplateColumns: `repeat(${menu.length}, 1fr)`,
+                width: '12em',
+                marginLeft: 'auto',
+              }}
+              data-testid="nek-header-menu"
+            >
+              {menu.map((menuItem) => {
+                const { link, text, ButtonProps = {} } = menuItem;
+                return (
+                  <Button
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    {...ButtonProps}
+                    onClick={() => navigate(link)}
+                    key={text}
+                    data-testid="nek-header-nav-button"
+                  >
+                    <Typography>{text}</Typography>
+                  </Button>
+                );
+              })}
+            </ButtonGroup>
+          </GridCenterContents>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
