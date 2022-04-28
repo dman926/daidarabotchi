609c551f2acc914bc358e8926b7625c6ac18a55e
Dennis James Stelmach <dj@dstelmach.com>
2022-04-28 19:57:22 -0400

    add media queries to header and home to fix overflow and bad image sizing

--- apps/new-england-keeshonds/src/app/pages/home/home.tsx
+++ apps/new-england-keeshonds/src/app/pages/home/home.tsx
@@ -5,14 +5,15 @@ import {
   Login,
   Page,
 } from '@daidarabotchi/new-england-keeshonds-lib';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 import AllDogsImage from '../../../assets/dogs/all_dogs.jpg';
 import WillowImage from '../../../assets/dogs/willow.jpg';
 import MiloImage from '../../../assets/dogs/milo.jpg';
 import BellaImage from '../../../assets/dogs/bella.jpg';
 
-// TODO: fill out the description more
-// TOOD: make this dynamic through functions
+// @TODO: fill out the description more
+// @TOOD: make this dynamic through functions
 const DOGS: {
   name: string;
   description: string;
@@ -38,6 +39,10 @@ const DOGS: {
 ];
 
 export function Home() {
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.down('md'));
+  const sm = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <Page testid="home-wrapper">
       <Container>
@@ -47,55 +52,64 @@ export function Home() {
               src={AllDogsImage}
               alt="Willow, Milo, and Bella"
               placeholder="blur"
-              style={{ objectFit: 'cover', objectPosition: 'center', width: '100%' }}
+              style={{
+                objectFit: 'cover',
+                objectPosition: 'center',
+                width: '100%',
+              }}
             />
           }
         >
-          {DOGS.map((dog, index) => (
-            <Grid container key={index}>
-              <Grid item xs={4}>
-                <Box
-                  sx={{
-                    display: 'grid',
-                    placeContent: 'center',
-                    paddingBottom: '0.25em',
-                    paddingRight: '0.25em',
-                  }}
-                >
-                  <img
-                    src={dog.img}
-                    alt={`${dog.name}`}
-                    placeholder="blur"
-                    width={256}
-                    height={256}
-                    style={{ objectFit: 'cover', objectPosition: 'center' }}
-                  />
-                </Box>
+          {DOGS.map((dog) => {
+            let imgSize = 256;
+            if (sm) {
+              imgSize = 64;
+            } else if (md) {
+              imgSize = 128;
+            }
+
+            return (
+              <Grid container key={dog.name}>
+                <Grid item xs={4}>
+                  <Box
+                    sx={{
+                      display: 'grid',
+                      placeContent: 'center',
+                      paddingBottom: '0.25em',
+                      paddingRight: '0.25em',
+                      height: '100%',
+                    }}
+                  >
+                    <img
+                      src={dog.img}
+                      alt={`${dog.name}`}
+                      placeholder="blur"
+                      width={imgSize}
+                      height={imgSize}
+                      style={{ objectFit: 'cover', objectPosition: 'center' }}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="h4" sx={{ marginTop: sm ? 0 : 1 }}>
+                    {dog.name}
+                  </Typography>
+                  <Typography>{dog.description}</Typography>
+                </Grid>
               </Grid>
-              <Grid item xs={8}>
-                <Typography variant="h4" sx={{ marginTop: 1 }}>
-                  {dog.name}
-                </Typography>
-                <Typography>{dog.description}</Typography>
-              </Grid>
-            </Grid>
-          ))}
+            );
+          })}
         </CallToAction>
-      </Container>
-      <Container maxWidth={false}>
-        <Typography align="center">
-          I don't know what to put down here. I was thinking of putting a full
-          width thing here. Probably just list the current litter if there is
-          one and a fallback message if not.
-        </Typography>
       </Container>
       <Container maxWidth="xs">
         <ContactForm head="Get A Hold Of Me" />
       </Container>
       <Container maxWidth="xs">
         <Login
-          head="Looking to make a paymnet? Enter your secret word below,"
-          onSubmit={(word) => {}}
+          head="Looking to make a payment? Enter your secret word below."
+          onSubmit={(word) => {
+            console.log(word);
+          }}
         />
       </Container>
     </Page>
