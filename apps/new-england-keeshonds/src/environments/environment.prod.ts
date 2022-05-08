506e77982ddaddfbb97e28641066935c4c54cc37
Dennis James Stelmach <dj@dstelmach.com>
2022-05-08 13:53:30 -0400

    fix exposed API keys :numbskull:

--- apps/new-england-keeshonds/src/environments/environment.prod.ts
+++ apps/new-england-keeshonds/src/environments/environment.prod.ts
@@ -1,17 +1,10 @@
 import { indigo, blueGrey } from '@mui/material/colors';
+import { firebaseProdConfig } from './secrets';
 
 // eslint-disable-next-line import/prefer-default-export
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCgMbTcvl7CtZV_Lu_MaFABhojbiT0sSo0',
-    authDomain: 'new-england-keeshonds.firebaseapp.com',
-    projectId: 'new-england-keeshonds',
-    storageBucket: 'new-england-keeshonds.appspot.com',
-    messagingSenderId: '32046810268',
-    appId: '1:32046810268:web:9fd8f49ced2346c6f41959',
-    measurementId: 'G-SMX2BBJBG3',
-  },
+  firebaseConfig: firebaseProdConfig
   baseTheme: {
     palette: {
       primary: indigo,
--- apps/new-england-keeshonds/src/environments/environment.ts
+++ apps/new-england-keeshonds/src/environments/environment.ts
@@ -1,4 +1,5 @@
 import { indigo, blueGrey } from '@mui/material/colors';
+import { firebaseDevConfig } from './secrets';
 
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
@@ -6,15 +7,7 @@ import { indigo, blueGrey } from '@mui/material/colors';
 // eslint-disable-next-line import/prefer-default-export
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCgMbTcvl7CtZV_Lu_MaFABhojbiT0sSo0',
-    authDomain: 'new-england-keeshonds.firebaseapp.com',
-    projectId: 'new-england-keeshonds',
-    storageBucket: 'new-england-keeshonds.appspot.com',
-    messagingSenderId: '32046810268',
-    appId: '1:32046810268:web:9fd8f49ced2346c6f41959',
-    measurementId: 'G-SMX2BBJBG3',
-  },
+  firebaseConfig: firebaseDevConfig,
   baseTheme: {
     palette: {
       primary: indigo,
