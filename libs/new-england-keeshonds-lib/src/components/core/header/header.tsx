ab175987271779451ec74f5dff29b8e51600b874
DJ Stelmach <dj@dstelmach.com>
2022-05-09 20:47:29 -0400

    fix header overflow

--- libs/new-england-keeshonds-lib/src/components/core/header/header.tsx
+++ libs/new-england-keeshonds-lib/src/components/core/header/header.tsx
@@ -4,13 +4,16 @@ import {
   ButtonGroup,
   ButtonProps as MuiButtonProps,
   Grid,
+  IconButton,
+  MenuItem,
   Toolbar,
   Typography,
 } from '@daidarabotchi/material-ui';
-import { useMediaQuery } from '@mui/material';
+import { Menu, useMediaQuery } from '@mui/material';
 import { styled, useTheme } from '@mui/material/styles';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { NavigateOptions } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const GridCenterContents = styled(Grid)(() => ({
   display: 'grid',
@@ -31,9 +34,19 @@ export interface HeaderProps {
 }
 
 export function Header(props: HeaderProps) {
+  const [mobileMenuAnchor, setMobileMenuAnchor] = useState<
+    HTMLElement | undefined
+  >();
   const theme = useTheme();
-  const sm = useMediaQuery(theme.breakpoints.down('sm'));
+  const xs = useMediaQuery(theme.breakpoints.only('xs'));
   const { navigate, title, currentRoute, home = '', menu } = props;
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMenuAnchor(event.currentTarget);
+  };
+  const handleClose = () => {
+    setMobileMenuAnchor(undefined);
+  };
 
   return (
     <AppBar position="static" data-testid="nek-header">
@@ -63,39 +76,68 @@ export function Header(props: HeaderProps) {
               )}
             </Typography>
           </GridCenterContents>
-          {!sm && (
-            <>
-              <GridCenterContents item xs={1} sx={{ position: 'absolute' }}>
-                {/* @TODO: PUT AN ANNOUNCEMENT BAR HERE WHEN THERE IS A CURRENT LITTER */}
-              </GridCenterContents>
-              <GridCenterContents item xs={6}>
-                <ButtonGroup
-                  sx={{
-                    display: 'grid',
-                    gridTemplateColumns: `repeat(${menu.length}, 1fr)`,
-                    width: '12em',
-                    marginLeft: 'auto',
-                  }}
-                  data-testid="nek-header-menu"
+          <GridCenterContents item xs={1} sx={{ position: 'absolute' }}>
+            {/* @TODO: PUT AN ANNOUNCEMENT BAR HERE WHEN THERE IS A CURRENT LITTER */}
+          </GridCenterContents>
+          <GridCenterContents item xs={6}>
+            {xs ? (
+              <>
+                <IconButton onClick={handleClick} sx={{ marginLeft: 'auto' }}>
+                  <MenuIcon htmlColor="white" />
+                </IconButton>
+                <Menu
+                  anchorEl={mobileMenuAnchor}
+                  open={Boolean(mobileMenuAnchor)}
+                  onClose={handleClose}
+                  transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
                 >
                   {menu.map((menuItem) => {
-                    const { link, text, ButtonProps = {} } = menuItem;
+                    const { link, text } = menuItem;
                     return (
-                      <Button
-                        // eslint-disable-next-line react/jsx-props-no-spreading
-                        {...ButtonProps}
-                        onClick={() => navigate(link)}
+                      <MenuItem
+                        onClick={() => {
+                          navigate(link);
+                          handleClose();
+                        }}
                         key={text}
                         data-testid="nek-header-nav-button"
                       >
                         <Typography>{text}</Typography>
-                      </Button>
+                      </MenuItem>
                     );
                   })}
-                </ButtonGroup>
-              </GridCenterContents>
-            </>
-          )}
+                </Menu>
+              </>
+            ) : (
+              <ButtonGroup
+                sx={{
+                  display: 'grid',
+                  gridTemplateColumns: `repeat(${menu.length}, 1fr)`,
+                  width: '12em',
+                  marginLeft: 'auto',
+                }}
+                data-testid="nek-header-menu"
+              >
+                {menu.map((menuItem) => {
+                  const { link, text, ButtonProps = {} } = menuItem;
+                  return (
+                    <Button
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...ButtonProps}
+                      onClick={() => {
+                        navigate(link);
+                      }}
+                      key={text}
+                      data-testid="nek-header-nav-button"
+                    >
+                      <Typography>{text}</Typography>
+                    </Button>
+                  );
+                })}
+              </ButtonGroup>
+            )}
+          </GridCenterContents>
         </Grid>
       </Toolbar>
     </AppBar>
