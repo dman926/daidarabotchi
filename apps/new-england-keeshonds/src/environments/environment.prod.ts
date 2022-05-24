4a06b21401ad6a6619bb3816e7dc3eee90756bf1
Dennis James Stelmach <dj@dstelmach.com>
2022-05-23 22:49:00 -0400

    change nek firebase env vars for github actions

--- apps/new-england-keeshonds/src/environments/environment.prod.ts
+++ apps/new-england-keeshonds/src/environments/environment.prod.ts
@@ -3,12 +3,9 @@ import { indigo, blueGrey } from '@mui/material/colors';
 // eslint-disable-next-line import/prefer-default-export
 export const environment = {
   production: true,
-  firebaseConfig: process.env['NX_FIREBASE_CONFIG']
-    ? JSON.parse(process.env['NX_FIREBASE_CONFIG'])
+  firebaseConfig: process.env['NX_NEK_FIREBASE_CONFIG']
+    ? JSON.parse(process.env['NX_NEK_FIREBASE_CONFIG'])
     : false,
-  firebaseServiceAccount: process.env['NX_FIREBASE_ADMIN_SDK']
-    ? JSON.parse(process.env['NX_FIREBASE_ADMIN_SDK'])
-    : undefined,
   baseTheme: {
     palette: {
       primary: indigo,
