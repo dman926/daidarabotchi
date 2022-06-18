8ec07a83b9a50407758c7f3299a3e2e178920dd0
Dennis James Stelmach <dj@dstelmach.com>
2022-06-17 21:40:29 -0400

    make generator not run pipenv commands if in action runner

--- libs/nx-python/src/generators/application/generator.ts
+++ libs/nx-python/src/generators/application/generator.ts
@@ -68,42 +68,46 @@ export default async function (
   tree: Tree,
   options: ApplicationGeneratorSchema
 ) {
+  // eslint-disable-next-line @typescript-eslint/dot-notation
+  const inGithubActions = process.env['GITHUB_ACTIONS'];
   // See if pipenv, setuptools, wheel, and watchman (if they want a type checker) are installed
   const exitFlag = {
     soft: false,
     hard: false,
   };
-  const cmds = [];
-  if (options.typeChecker !== 'none') {
-    cmds.push('watchman');
-  }
-  cmds.forEach((cmd) => {
-    if (!commandExistsSync(cmd)) {
-      logger.warn(`${cmd} missing from PATH`);
-      exitFlag.soft = true;
-      exitFlag.hard = true;
+  if (!inGithubActions) {
+    const cmds = [];
+    if (options.typeChecker !== 'none') {
+      cmds.push('watchman');
     }
-  });
-  if (exitFlag.soft) {
-    logger.error(
-      'Please install the above programs with your OS package manager'
-    );
-    exitFlag.soft = false;
-  }
-  ['setuptools', 'wheel', 'pipenv'].forEach((cmd) => {
-    try {
-      execSync(`python3 -c "import ${cmd}"`);
-    } catch (e) {
-      logger.warn(`${cmd} not in python environment`);
-      exitFlag.soft = true;
-      exitFlag.hard = true;
+    cmds.forEach((cmd) => {
+      if (!commandExistsSync(cmd)) {
+        logger.warn(`${cmd} missing from PATH`);
+        exitFlag.soft = true;
+        exitFlag.hard = true;
+      }
+    });
+    if (exitFlag.soft) {
+      logger.error(
+        'Please install the above programs with your OS package manager'
+      );
+      exitFlag.soft = false;
     }
-  });
-  if (exitFlag.soft) {
-    logger.error('Please install the above packages with pip');
-  }
-  if (exitFlag.hard) {
-    return () => {};
+    ['setuptools', 'wheel', 'pipenv'].forEach((cmd) => {
+      try {
+        execSync(`python3 -c "import ${cmd}"`);
+      } catch (e) {
+        logger.warn(`${cmd} not in python environment`);
+        exitFlag.soft = true;
+        exitFlag.hard = true;
+      }
+    });
+    if (exitFlag.soft) {
+      logger.error('Please install the above packages with pip');
+    }
+    if (exitFlag.hard) {
+      return () => {};
+    }
   }
   const normalizedOptions = normalizeOptions(tree, options);
   addProjectConfiguration(tree, normalizedOptions.projectName, {
@@ -138,32 +142,6 @@ export default async function (
   addFiles(tree, normalizedOptions);
   await formatFiles(tree);
   return () => {
-    // pipenv install frequently used dependencies (pipenv-setup [DEV]) (auto creates environment)
-    // pipenv install a code formatter (nothing, black, autopep8) (DEV) (optional)
-    // pipenv install a test runner (nothing for unittest, robot, pytest) (DEV) (optional)
-    // pipenv install a type checker (mypi, pyright, pytype, pyre) (DEV) (optional)
-    const packages = ['setuptools', 'wheel'];
-    if (normalizedOptions.formatter !== 'none') {
-      packages.push(normalizedOptions.formatter);
-    }
-    if (normalizedOptions.testRunner !== 'none') {
-      let testRunnerPackage: string;
-      if (normalizedOptions.testRunner === 'robot') {
-        testRunnerPackage = 'robotframework';
-      } else {
-        testRunnerPackage = normalizedOptions.testRunner;
-      }
-      packages.push(testRunnerPackage);
-    }
-    if (normalizedOptions.typeChecker !== 'none') {
-      let typeCheckerPackage: string;
-      if (normalizedOptions.typeChecker === 'pyre') {
-        typeCheckerPackage = 'pyre-check';
-      } else {
-        typeCheckerPackage = normalizedOptions.typeChecker;
-      }
-      packages.push(typeCheckerPackage);
-    }
     const context = {
       workspace: {
         projects: {
@@ -177,75 +155,103 @@ export default async function (
     const cwd = `${process.cwd()}/${
       context.workspace.projects[context.projectName].root
     }`;
-    logger.info(`Executing command: pipenv install`);
-    execSync('pipenv install', { cwd });
-    if (normalizedOptions.allowPre) {
-      runPipenvCommand(context, 'install --pre');
-    }
 
-    const format = normalizedOptions.formatter !== 'none';
-    let formatCmd: string;
-    const lint = normalizedOptions.typeChecker !== 'none';
-    let lintCmd: string;
-    let testCmd: string;
+    if (!inGithubActions) {
+      // pipenv install frequently used dependencies (pipenv-setup [DEV]) (auto creates environment)
+      // pipenv install a code formatter (nothing, black, autopep8) (DEV) (optional)
+      // pipenv install a test runner (nothing for unittest, robot, pytest) (DEV) (optional)
+      // pipenv install a type checker (mypi, pyright, pytype, pyre) (DEV) (optional)
+      const packages = ['setuptools', 'wheel'];
+      if (normalizedOptions.formatter !== 'none') {
+        packages.push(normalizedOptions.formatter);
+      }
+      if (normalizedOptions.testRunner !== 'none') {
+        let testRunnerPackage: string;
+        if (normalizedOptions.testRunner === 'robot') {
+          testRunnerPackage = 'robotframework';
+        } else {
+          testRunnerPackage = normalizedOptions.testRunner;
+        }
+        packages.push(testRunnerPackage);
+      }
+      if (normalizedOptions.typeChecker !== 'none') {
+        let typeCheckerPackage: string;
+        if (normalizedOptions.typeChecker === 'pyre') {
+          typeCheckerPackage = 'pyre-check';
+        } else {
+          typeCheckerPackage = normalizedOptions.typeChecker;
+        }
+        packages.push(typeCheckerPackage);
+      }
+      logger.info(`Executing command: pipenv install`);
+      execSync('pipenv install', { cwd });
+      if (normalizedOptions.allowPre) {
+        runPipenvCommand(context, 'install --pre');
+      }
 
-    if (format) {
-      switch (normalizedOptions.formatter) {
-        case 'autopep8':
-          formatCmd = 'autopep8 --in-place --recursive .';
+      const format = normalizedOptions.formatter !== 'none';
+      let formatCmd: string;
+      const lint = normalizedOptions.typeChecker !== 'none';
+      let lintCmd: string;
+      let testCmd: string;
+
+      if (format) {
+        switch (normalizedOptions.formatter) {
+          case 'autopep8':
+            formatCmd = 'autopep8 --in-place --recursive .';
+            break;
+          case 'black':
+            formatCmd = 'black .';
+            break;
+          default:
+        }
+      }
+      if (lint) {
+        switch (normalizedOptions.typeChecker) {
+          case 'mypy':
+            lintCmd = 'mypy tests';
+            break;
+          case 'pyright':
+            lintCmd = 'pyright --watch .';
+            break;
+          case 'pytype':
+            lintCmd = 'pytype .';
+            break;
+          case 'pyre':
+            lintCmd = 'pyre';
+            break;
+          default:
+        }
+      }
+      switch (normalizedOptions.testRunner) {
+        case 'none':
+          testCmd = 'unittest discover -s ./ -p';
           break;
-        case 'black':
-          formatCmd = 'black .';
+        case 'pytest':
+          testCmd = 'pytest';
+          break;
+        case 'robot':
+          testCmd = 'robot .';
           break;
         default:
       }
-    }
-    if (lint) {
-      switch (normalizedOptions.typeChecker) {
-        case 'mypy':
-          lintCmd = 'mypy tests';
-          break;
-        case 'pyright':
-          lintCmd = 'pyright --watch .';
-          break;
-        case 'pytype':
-          lintCmd = 'pytype .';
-          break;
-        case 'pyre':
-          lintCmd = 'pyre';
-          break;
-        default:
+      // const pipfile = readFileSync(`${cwd}/Pipfile`).toString().split('\n');
+      const commands = [
+        '[scripts]',
+        `build = "setup.py sdist bdist_wheel --dist-dir ../../dist/${normalizedOptions.projectName}"`,
+      ];
+      if (format) {
+        commands.push(`format = "python3 -m ${formatCmd}"`);
       }
+      if (lint) {
+        commands.push(`lint = "python3 -m ${lintCmd}"`);
+      }
+      commands.push(
+        `test = "python3 -m ${testCmd}"`,
+        'serve = "python3 main.py"'
+      );
+      appendFileSync(`${cwd}/Pipfile`, `\n${commands.join('\n')}\n`);
+      runPipenvCommand(context, `install --dev ${packages.join(' ')}`);
     }
-    switch (normalizedOptions.testRunner) {
-      case 'none':
-        testCmd = 'unittest discover -s ./ -p';
-        break;
-      case 'pytest':
-        testCmd = 'pytest';
-        break;
-      case 'robot':
-        testCmd = 'robot .';
-        break;
-      default:
-    }
-    // const pipfile = readFileSync(`${cwd}/Pipfile`).toString().split('\n');
-    const commands = [
-      '[scripts]',
-      `build = "setup.py sdist bdist_wheel --dist-dir ../../dist/${normalizedOptions.projectName}"`,
-    ];
-    if (format) {
-      commands.push(`format = "python3 -m ${formatCmd}"`);
-    }
-    if (lint) {
-      commands.push(`lint = "python3 -m ${lintCmd}"`);
-    }
-    commands.push(
-      `test = "python3 -m ${testCmd}"`,
-      'serve = "python3 main.py"'
-    );
-    // pipfile.splice(5, 0, `${commands.join('\n')}\n`);
-    appendFileSync(`${cwd}/Pipfile`, `\n${commands.join('\n')}\n`);
-    runPipenvCommand(context, `install --dev ${packages.join(' ')}`);
   };
 }
