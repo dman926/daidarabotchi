acdfa987fd3086bb565af3606890826d2a1a372c
Dennis James Stelmach <dj@dstelmach.com>
2022-06-19 01:01:45 -0400

    regen firebase-react with swc instead of babel

--- libs/firebase-react/jest.config.ts
+++ libs/firebase-react/jest.config.ts
@@ -3,7 +3,10 @@ export default {
   displayName: 'firebase-react',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/firebase-react',
