fd34033c91e327138bf58a3051f7e5824431381b
Dennis James Stelmach <dj@dstelmach.com>
2022-06-19 22:28:23 -0400

    add basic header component

--- apps/portfolio/src/app/app.tsx
+++ apps/portfolio/src/app/app.tsx
@@ -1,5 +1,22 @@
+import { Container } from '@daidarabotchi/material-ui';
+import { Header, HeaderProps } from '@daidarabotchi/portfolio-lib';
+
+const headerProps: HeaderProps = {
+  name: {
+    firstName: 'Dennis James',
+    lastName: 'Stelmach',
+    suffix: 'JR',
+    nickName: '(DJ)',
+  },
+};
+
 export function App() {
-  return <h1>test</h1>;
+  return (
+    <Container>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Header {...headerProps} />
+    </Container>
+  );
 }
 
 export default App;
