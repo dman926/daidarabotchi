64bef945836e67f04a3688866af4e52114a93494
Dennis James Stelmach <dj@dstelmach.com>
2022-06-19 23:25:12 -0400

    add routing. move person schema scope to app level

---
+++ apps/portfolio/src/app/resume/resume.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+
+import { Resume } from './resume';
+
+describe('App', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Resume />);
+
+    expect(baseElement).toBeTruthy();
+  });
+});
