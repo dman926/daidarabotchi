96f1350cd02fbfee376ad3da70de2b2b72d852dc
Dennis James Stelmach <dj@dstelmach.com>
2022-06-21 00:28:55 -0400

    fix test

--- apps/portfolio/src/app/app.spec.tsx
+++ apps/portfolio/src/app/app.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { App } from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
 
     expect(baseElement).toBeTruthy();
   });
