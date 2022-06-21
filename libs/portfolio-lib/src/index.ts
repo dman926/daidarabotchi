eb0a500e029d75d788217a1ba038c914d5708354
Dennis James Stelmach <dj@dstelmach.com>
2022-06-21 01:19:30 -0400

    add work experience organization schema

--- libs/portfolio-lib/src/index.ts
+++ libs/portfolio-lib/src/index.ts
@@ -2,3 +2,4 @@ export * from './components/experience/generic-experience/generic-experience';
 export * from './components/experience/project-experience/project-experience';
 export * from './components/experience/work-experience/work-experience';
 export * from './components/header/header';
+export * from './models';
