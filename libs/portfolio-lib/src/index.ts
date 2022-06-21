01fd65e8f2ff98c84ef065af0720467281811498
Dennis James Stelmach <dj@dstelmach.com>
2022-06-21 12:46:26 -0400

    export contact. remove header-contact

--- libs/portfolio-lib/src/index.ts
+++ libs/portfolio-lib/src/index.ts
@@ -1,3 +1,4 @@
+export * from './components/contact/contact';
 export * from './components/experience/generic-experience/generic-experience';
 export * from './components/experience/project-experience/project-experience';
 export * from './components/experience/work-experience/work-experience';
