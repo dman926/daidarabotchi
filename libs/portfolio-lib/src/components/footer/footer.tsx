5329f1eb93d388d6e34e4696dc33522f9348caaf
Dennis James Stelmach <dj@dstelmach.com>
2022-06-21 12:41:41 -0400

    move contact to it's own component

--- libs/portfolio-lib/src/components/footer/footer.tsx
+++ libs/portfolio-lib/src/components/footer/footer.tsx
@@ -1,10 +1,18 @@
 import { Box } from '@daidarabotchi/material-ui';
+import { Contact, ContactProps } from '../contact/contact';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface FooterProps {}
+export interface FooterProps {
+  contact?: ContactProps;
+}
 
-export function Footer(props: FooterProps) {
-  return <Box data-testid="portfolio-footer">Footer</Box>;
+export function Footer({ contact }: FooterProps) {
+  return (
+    <Box data-testid="portfolio-footer">
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      {contact && <Contact {...contact} />}
+    </Box>
+  );
 }
 
 export default Footer;
