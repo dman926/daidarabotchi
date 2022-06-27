cd73875f54d621a06c66ef27cc1c2b32a4f2c9c1
Dennis James Stelmach <dj@dstelmach.com>
2022-06-27 12:06:48 -0400

    add props to generic experience and display them

--- libs/portfolio-lib/src/components/experience/generic-experience/generic-experience.tsx
+++ libs/portfolio-lib/src/components/experience/generic-experience/generic-experience.tsx
@@ -6,48 +6,76 @@ import {
   CardContent,
   CardHeader,
   CardMedia,
-  CardHeaderProps,
-  CardMediaProps,
+  Typography,
+  Paper,
 } from '@daidarabotchi/material-ui';
+import { useMemo } from 'react';
+import { formatStartEndDate } from '../../../utils';
 
 export interface GenericExperienceProps {
-  short?: {
-    header: {
-      headerProps: CardHeaderProps;
-      testid?: string;
-    };
-    media?: {
-      mediaProps: CardMediaProps;
-      testid?: string;
-    };
-    content?: {
-      contentBody: JSX.Element | string;
-      testid?: string;
-    };
-  };
+  title: string;
+  subheader?: string;
+  content: string;
+  media?: string[];
+  startDate?: Date;
+  endDate?: Date;
+  noPresent?: boolean;
+  location?: string;
+  compact?: boolean;
 }
 
-export function GenericExperience({ short }: GenericExperienceProps) {
-  const { header, media, content } = short || {};
-  const { headerProps, testid: headerTestId } = header || {};
-  const { mediaProps, testid: mediaTestId } = media || {
-    mediaProps: undefined,
-  };
-  const { contentBody, testid: contentTestId } = content || {
-    contentBody: undefined,
-  };
+export function GenericExperience({
+  title,
+  subheader,
+  content,
+  media,
+  startDate,
+  endDate,
+  noPresent,
+  location,
+  compact,
+}: GenericExperienceProps) {
+  const cachedSubheader = useMemo(
+    () =>
+      `${subheader || formatStartEndDate(startDate, endDate, noPresent)}${
+        location && ` - ${location}`
+      }`,
+    [endDate, location, noPresent, startDate, subheader]
+  );
+
   return (
     <Box data-testid="portfolio-generic-experience">
-      {short && (
-        <Card data-testid="portfolio-generic-experience-short">
-          <CardHeader {...headerProps} data-testid={headerTestId} />
-          {mediaProps && (
-            <CardMedia {...mediaProps} data-testid={mediaTestId} />
+      {compact ? (
+        <Card data-testid="portfolio-generic-experience-compact">
+          <CardHeader
+            title={title}
+            subheader={cachedSubheader}
+            data-testid="portfolio-generic-experience-header"
+          />
+          {media && (
+            <CardMedia
+              // @TODO: convert this into a carousel
+              image={media[0]}
+              data-testid="portfolio-generic-experience-media"
+            />
           )}
-          {contentBody && (
-            <CardContent data-testid={contentTestId}>{contentBody}</CardContent>
-          )}
+          <CardContent data-testid="portfolio-generic-experience-content">
+            {content}
+          </CardContent>
         </Card>
+      ) : (
+        <Paper data-testid="portfolio-generic-experience-long">
+          <Typography data-testid="portfolio-generic-experience-header">
+            {title}
+          </Typography>
+          <Typography data-testid="portfolio-generic-experience-subheader">
+            {cachedSubheader}
+          </Typography>
+          {/* @TODO: carousel here */}
+          <Typography data-testid="portfolio-generic-experience-content">
+            {content}
+          </Typography>
+        </Paper>
       )}
     </Box>
   );
