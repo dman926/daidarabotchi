5e90254c44afd078445d6d82cab73d65ab201db5
Dennis James Stelmach <dj@dstelmach.com>
2022-08-06 02:48:29 -0400

    add user table migration

---
+++ apps/sephira-api/migrations/20220806014826_create_auth_tables.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+CREATE TABLE "user"(
+  id VARCHAR(64) NOT NULL,
+  passwd CHAR(60) NOT NULL,
+  CONSTRAINT pk_user PRIMARY KEY (id)
+);
+CREATE TABLE "user_claims"(
+  userid VARCHAR(64) NOT NULL,
+  claim_name VARCHAR(32) NOT NULL,
+  claim_value VARCHAR(32) NOT NULL,
+  CONSTRAINT pk_user_claims PRIMARY KEY (userid, claim_name),
+  CONSTRAINT fk_user_claims_user FOREIGN KEY (userid) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+COMMIT;
