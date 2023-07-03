6d5c9eeed04b9834d3884150d7e62fb58305ef03
Dennis James Stelmach <dj@dstelmach.com>
2023-07-03 02:59:55 -0400

    Update photo function stubs. Add TODOs.

--- packages/new-england-keeshonds/firebase/functions/photos/index.ts
+++ packages/new-england-keeshonds/firebase/functions/photos/index.ts
@@ -1,9 +1,3 @@
-/**
- * @TODO:
- *    Add a function to retrieve an array of ImagePackage from Google Photos and any albums that are shared
- *    Add functions to control viewing Google Photos albums and adding and removing read permissions from specific albums
- */
-
 import { onCall } from 'firebase-functions/v2/https';
 import { onSchedule } from 'firebase-functions/v2/scheduler';
 
@@ -14,28 +8,42 @@ interface ImagePackage {
 
 const everyDayAtMidnightCron = '0 0 * * *';
 
+// @TODO: Add util function to compare stored album and source album and apply differences
+
 export const albumSyncHandler = onSchedule(everyDayAtMidnightCron, (event) => {
+  /**
+   * @TODO
+   *  Refresh Firestore admin/photos.(refreshToken|provider)
+   *  Sync stored and source albums using Firestore admin/photos.albums using util function
+   */
   return;
 });
 
 export const adminGetAlbumsHandler = onCall(
   { enforceAppCheck: true },
   (request) => {
+    // @TODO: Get a list of all available albums (and main thumbnails) and all synced albums
+    return {};
+  }
+);
+
+export const adminSetAlbumsHandler = onCall(
+  { enforceAppCheck: true },
+  (request) => {
+    // @TODO: Set the list of synced albums and apply diffs with util function
     return {};
   }
 );
 
 export const getAlbumsHandler = onCall({ enforceAppCheck: true }, (request) => {
+  // @TODO: Get a list of album names and metadata and the first page of photos
   return {};
 });
 
-export const addAlbumHandler = onCall({ enforceAppCheck: true }, (request) => {
-  return {};
-});
-
-export const removeAlbumHandler = onCall(
+export const getPagedAlbumHandler = onCall(
   { enforceAppCheck: true },
   (request) => {
+    // @TODO: Handle continued pagination of an album
     return {};
   }
 );
